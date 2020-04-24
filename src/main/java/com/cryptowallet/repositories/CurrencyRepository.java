bf7566724ec64cbaa111b830cdcb4cdaa4ece3e8
AcidSore <INFomicheva@yandex.ru>
2020-04-24 12:26:48 +0300

    fixed

--- src/main/java/com/cryptowallet/repositories/CurrencyRepository.java
+++ src/main/java/com/cryptowallet/repositories/CurrencyRepository.java
@@ -2,6 +2,8 @@ package com.cryptowallet.repositories;
 
 import com.cryptowallet.entities.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 }
