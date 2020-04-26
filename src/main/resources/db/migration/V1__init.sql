eb9a1b9212a0a6cf7bb62b333619069eea0fe5fa
Dmitry Ozhigin <45883558+Lesnoytishka@users.noreply.github.com>
2020-04-26 21:12:54 +0300

    disable change V1__initialDB

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id                    BIGSERIAL,
-  login                 VARCHAR(255) NOT NULL UNIQUE,
-  password              VARCHAR(255),
-  email                 VARCHAR(255) UNIQUE,
-  token                 VARCHAR(255),
+  login                 VARCHAR(30) NOT NULL UNIQUE,
+  password              VARCHAR(30),
+  email                 VARCHAR(30) UNIQUE,
+  token                 VARCHAR(50),
   approved              BOOL,
   PRIMARY KEY (id)
 );
