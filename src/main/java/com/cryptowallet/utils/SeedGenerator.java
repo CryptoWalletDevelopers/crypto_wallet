ee1a54b737b697f18598a985eb4beadacc4dc9d2
AcidSore <INFomicheva@yandex.ru>
2020-04-30 21:40:27 +0300

    fixed getting address

--- src/main/java/com/cryptowallet/utils/SeedGenerator.java
+++ src/main/java/com/cryptowallet/utils/SeedGenerator.java
@@ -1,12 +1,12 @@
 package com.cryptowallet.utils;
 
-import com.cryptowallet.crypto.Base58;
-import com.cryptowallet.crypto.ByteArray;
-import com.cryptowallet.crypto.ECKey;
-import com.cryptowallet.crypto.Sha256Hash;
+import com.cryptowallet.crypto.*;
+import com.cryptowallet.crypto.jce.TronCastleProvider;
+import com.cryptowallet.crypto.sm2.SM2;
 import io.github.novacrypto.bip32.derivation.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.core.util.JsonUtils;
 import org.spongycastle.asn1.sec.SECNamedCurves;
 import org.spongycastle.asn1.x9.X9ECParameters;
 import org.spongycastle.crypto.params.ECDomainParameters;
@@ -16,6 +16,12 @@ import io.github.novacrypto.bip32.ExtendedPrivateKey;
 import io.github.novacrypto.bip39.SeedCalculator;
 import io.github.novacrypto.bip32.networks.Bitcoin;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.security.PrivateKey;
+import java.util.Arrays;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
 
 @Component
 @Data
@@ -31,6 +37,11 @@ public  class SeedGenerator {
             hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
         return new String(hexChars);
+    }
+    public static byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
     }
 
 
@@ -51,16 +62,20 @@ public  class SeedGenerator {
                 .address(0);
 
         ExtendedPrivateKey accountPrivate = rootKey.derive(addressIndex.toString());
-        BigInteger pk = new BigInteger(bytesToHex(accountPrivate.extendedBase58().getBytes()), 16);
+        BigInteger pk = new BigInteger(bytesToHex(accountPrivate.extendedBase58().getBytes()).substring(0,64), 16);
 
-        ECKey key = new ECKey(accountPrivate.extendedBase58().getBytes(), true);
-        System.out.println(key.isPubKeyCanonical());
-        System.out.println(bytesToHex(key.getPubKey()));
+//        System.out.println(bytesToHex(accountPrivate.extendedBase58().getBytes()).substring(0,64));
+        ECKey key = ECKey.fromPrivate(pk);
+
+//        byte[] myKey = new byte[64];
+//        System.arraycopy(accountPrivate.extendedKeyByteArray(),0, myKey,0, 64);
+//
+//        ECKey key = ECKey.fromPrivate(ByteArray.fromHexString(bytesToHex(accountPrivate.extendedKeyByteArray())));
+
         System.out.println(bytesToHex(key.getPrivKeyBytes()));
-
         System.out.println(bytesToHex(key.getAddress()));
+        System.out.println(tronString(key.getAddress()));
 
-        System.out.println(tronHex("TFNVLMmwttCaBLkecWJQRAwWuDyjAxxyBK"));
 
 
 //        Account account = BIP44
@@ -96,14 +111,25 @@ public  class SeedGenerator {
         return null;
     }
 
+    public static byte[] checksum(byte[] arr){
+    Checksum checksum = new CRC32();
+    checksum.update(arr, 0,arr.length);
+    long checksumValue = checksum.getValue();
+    return longToBytes(checksumValue);
+    }
+
     private static String tronString(byte[] input){
         byte[] hash0 = Sha256Hash.hash(input);
         byte[] hash1 = Sha256Hash.hash(hash0);
+       // byte[] checksum = checksum(hash1);
+        byte[] input1 = new byte[input.length + 4];
+        System.arraycopy(input,0, input1,0, input.length);
+        System.arraycopy(hash1, 0, input1, input.length , 4);
 
         // посчитать checksum от hash1, взять 4 байта и добавить в конец к hash1
 //        byte[] checksum = Sha256Hash.hashTwice(input, 0, 4);
 //        System.arraycopy(checksum, 0, hash1, payload.length + 1, 4);
-
-        return Base58.encode(hash1);
+        return Base58.encode(input1);
     }
+
 }
