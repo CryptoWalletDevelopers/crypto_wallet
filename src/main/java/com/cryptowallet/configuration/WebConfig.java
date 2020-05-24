721367ae81f215e32519d6fb2aacb97b8afaa9ca
BreevE <e.m.breev@gmail.com>
2020-05-24 14:57:34 +0300

    added fragments by Yaroslav

--- src/main/java/com/cryptowallet/configuration/WebConfig.java
+++ src/main/java/com/cryptowallet/configuration/WebConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan("com.cryptowallet")
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/jquery/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/jquery/3.5.0/");
-
-        registry.addResourceHandler("/popper/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/popper.js/2.0.2/umd/");
-
-        registry.addResourceHandler("/bootstrap/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/4.4.1-1/");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/jquery/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/jquery/3.5.0/");
+//
+//        registry.addResourceHandler("/popper/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/popper.js/2.0.2/umd/");
+//
+//        registry.addResourceHandler("/bootstrap/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/4.4.1-1/");
+//    }
 }
