69f49f3be980a81725d5d51c1e71bde9b2904ede
BreevE <e.m.breev@gmail.com>
2020-05-26 02:09:34 +0300

    added modal windows

---
+++ src/main/resources/static/js/wallet.js
@@ -0,0 +1,13 @@
+$('#getModalWindow').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget)
+  var id = button.data('id')
+  var modal = $(this)
+  modal.find('.modal-body #get-id').val(id)
+})
+
+$('#sendModalWindow').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget)
+  var id = button.data('id')
+  var modal = $(this)
+  modal.find('.modal-body #send-id').val(id)
+})
